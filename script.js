68b653148b67b8db88b91eab4c330a36dd09a383
22DP3KBori <144322401+22DP3KBori@users.noreply.github.com>
2024-10-11 18:10:35 +0000

    Izveidoju visus uzdevumus un WEB saits ir beigss

--- script.js
+++ script.js
@@ -5,3 +5,53 @@ function openModal() {
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+
+    let darkMode = localStorage.getItem('darkMode');
+
+    const enableDarkMode = () => {
+        document.body.classList.add('dark-mode');
+        document.querySelector('header').classList.add('dark-mode');
+        document.querySelector('footer').classList.add('dark-mode');
+        document.querySelectorAll('.card').forEach(card => card.classList.add('dark-mode'));
+        document.querySelectorAll('.learn-more-btn').forEach(btn => btn.classList.add('dark-mode'));
+
+        darkModeToggle.classList.remove('fa-moon-o');
+        darkModeToggle.classList.add('fa-sun-o');
+
+        localStorage.setItem('darkMode', 'enabled');
+    };
+
+    const disableDarkMode = () => {
+        document.body.classList.remove('dark-mode');
+        document.querySelector('header').classList.remove('dark-mode');
+        document.querySelector('footer').classList.remove('dark-mode');
+        document.querySelectorAll('.card').forEach(card => card.classList.remove('dark-mode'));
+        document.querySelectorAll('.learn-more-btn').forEach(btn => btn.classList.remove('dark-mode'));
+
+        darkModeToggle.classList.remove('fa-sun-o');
+        darkModeToggle.classList.add('fa-moon-o');
+
+        localStorage.setItem('darkMode', 'disabled');
+    };
+
+    if (darkMode === 'enabled') {
+        enableDarkMode();
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        darkMode = localStorage.getItem('darkMode');
+        if (darkMode !== 'enabled') {
+            enableDarkMode();
+        } else {
+            disableDarkMode();
+        }
+    });
+});
+
+
+
+
